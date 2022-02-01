86d73cdf1510e60d2333edf468be08f7818fc692
itsalireza.zr <itsalireza.zr@gmail.com>
2022-02-01 13:52:29 +0330

    new error boundaries tested successfully!

--- sample/io_tst.py
+++ sample/io_tst.py
@@ -1,15 +1,12 @@
 """ WRITE YOUR OWN CORRECT CODE TO TEST THE OUTPUT """
 
-import random
 from constants.settings import NUM_OF_TESTS
 
 
 def main_code(test_index: int = 0):
     """ RETURN THE I/O THAT IS COMPATIBLE WITH YOUR TEST """
-    n = random.randint(1, 1000)
-    arr = [random.randint(1, 100000) for _ in range(n)]
-    
-    i = f"{n}\n{' '.join(list(map(str, arr)))}"  # some input
-    o = f"{max(arr)}\n"  # some output
+
+    i = f""  # some input
+    o = f""  # some output
 
     return i, o
--- sample/test_dev.py
+++ sample/test_dev.py
@@ -1,8 +1,11 @@
+from constants.COLORS import *
 from sample.io_tst import main_code
 
 
 def test_main_code():
+    print(f"{OKCYAN}")
     i, o = main_code()
+    print(f"{ENDC}")
 
     print(f"inputs:\n{i}")
     print(f"outputs:\n{o}")
