0915f01634b72d60e3d5335363246af642eb4fc2
Alireza Zahiri <itsalireza.zr@gmail.com>
2022-02-13 23:13:58 +0330

    threads added for easier processing...

--- combo.py
+++ combo.py
@@ -3,20 +3,36 @@ import os
 from constants.COLORS import *
 from constants.settings import INPUT_FILES_ROOT, SCORE_STATISTICS_FILE, TARGET_ROOT
 from core.run_samples import execute
-
+import threading
 
 files = os.listdir(INPUT_FILES_ROOT)
 with open(SCORE_STATISTICS_FILE, "w") as initialize:
     print(end='file name,coverage\n', file=initialize)
 initialize.close()
 
-for file in files:
-    if file.split('.')[1] in ['c', 'cpp']:
-        print(f"{WARNING}{BOLD}RUNNING TESTS FOR {file}...{ENDC}")
-        shutil.copyfile(rf"{INPUT_FILES_ROOT}/{file}",
-                        rf"{TARGET_ROOT}/main.cpp")
-        response = execute(f"{TARGET_ROOT}/main.cpp", file)
-        print(f"{WARNING}{BOLD}FINISHED TESTING PROCESS ON {file}{ENDC}")
-        with open(SCORE_STATISTICS_FILE, "a") as scores_file:
-            print(response, file=scores_file)
-        scores_file.close()
+files = [file for file in files if file.split('.')[1] in ['c', 'cpp']]
+threads = []
+results = []
+
+
+def pre_execute(target_root: str, filename: str):
+    results.append(execute(target_root, filename))
+
+for index, file in enumerate(files):
+    print(f"{WARNING}{BOLD}RUNNING TESTS FOR {file}...{ENDC}")
+    
+    shutil.copyfile(rf"{INPUT_FILES_ROOT}/{file}",
+                    rf"{TARGET_ROOT}/main.cpp")
+    
+    thread = threading.Thread(target=pre_execute(f"{TARGET_ROOT}/main.cpp", file))
+    thread.start()
+    threads.append(thread)
+    
+    print(f"{WARNING}{BOLD}FINISHED TESTING PROCESS ON {file}{ENDC}")
+    
+    with open(SCORE_STATISTICS_FILE, "a") as scores_file:
+        print(results[index], file=scores_file)
+    scores_file.close()
+
+for thread in threads:
+    thread.join()
